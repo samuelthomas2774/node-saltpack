4f8b15fc375073f98f7a06b83ba239a0e21e1e80
Samuel Elliott <samuel@fancy.org.uk>
2023-10-30 15:55:46 +0000

    Add tests

--- src/__tests__/data/encryption-keys.ts
+++ src/__tests__/data/encryption-keys.ts
@@ -8,3 +8,8 @@ export const KEYPAIR_BOB = tweetnacl.box.keyPair.fromSecretKey(Uint8Array.from(B
     'aa3c626bc9c38c8c201878ebb1d5b0b50ac40e8986c78793db1d4ef369fca1ce', 'hex')));
 export const KEYPAIR_MALLORY = tweetnacl.box.keyPair.fromSecretKey(Uint8Array.from(Buffer.from(
     '98aebbb178a551876bfaf8e1e530dac6aaf6c2ea1c8f8406a3ab37dfb40fbc25', 'hex')));
+
+export const SYMMETRIC_KEY_BOB = Buffer.from(
+    'c5db2c6f47009e06f0a4e9e479f62c300436165ac98956b1a42e5c44e3a1ada7', 'hex');
+export const SYMMETRIC_KEY_MALLORY = Buffer.from(
+    'c6f6e3cdeda10449bb91841a141517baf0cd8c06e15843dcf201284ce41063f1', 'hex');
--- src/__tests__/signcryption.ts
+++ src/__tests__/signcryption.ts
@@ -1,11 +1,12 @@
 
 import { signcrypt, designcrypt, SigncryptStream, DesigncryptStream, debugSetKey, debugSetKeypair } from '../signcryption/index.js';
+import { SymmetricKeyRecipient } from '../signcryption/recipient.js';
 
 // With signcryption the sender *signs the message* with their Ed25519 key, and *encrypts for recipients'*
 // Curve25519 keys - so Alice, the sender, uses a signing key and Bob, the recipient, uses an encryption key
-import { KEYPAIR, /*KEYPAIR_ALICE,*/ KEYPAIR_BOB, KEYPAIR_MALLORY } from './data/encryption-keys.js';
+import { KEYPAIR, /*KEYPAIR_ALICE,*/ KEYPAIR_BOB, KEYPAIR_MALLORY, SYMMETRIC_KEY_BOB, SYMMETRIC_KEY_MALLORY } from './data/encryption-keys.js';
 import { KEYPAIR_ALICE /*, KEYPAIR_BOB as KEYPAIR_BOB_S */ } from './data/signing-keys.js';
-import { INPUT_STRING, SIGNCRYPTED } from './data/signcryption-tests.js';
+import { INPUT_STRING, SIGNCRYPTED, SIGNCRYPTED_SYMMETRIC } from './data/signcryption-tests.js';
 
 debugSetKey(Buffer.alloc(32).fill('\x00'));
 debugSetKeypair(KEYPAIR);
@@ -86,4 +87,67 @@ test('decrypt with wrong keypair fails', async () => {
     await expect(async () => {
         await designcrypt(SIGNCRYPTED, KEYPAIR_MALLORY);
     }).rejects.toThrow();
+});
+
+describe('symmetric key recipients', () => {
+    test('encrypt for symmetric key', async () => {
+        const encrypted = await signcrypt(INPUT_STRING, KEYPAIR_ALICE, [
+            new SymmetricKeyRecipient(Buffer.alloc(32), SYMMETRIC_KEY_BOB),
+        ]);
+
+        expect(encrypted).toStrictEqual(SIGNCRYPTED_SYMMETRIC);
+    });
+
+    test('decrypt with symmetric key', async () => {
+        const data = await designcrypt(SIGNCRYPTED_SYMMETRIC,
+            new SymmetricKeyRecipient(Buffer.alloc(32), SYMMETRIC_KEY_BOB),
+            KEYPAIR_ALICE.publicKey);
+    
+        expect(data.toString()).toBe(INPUT_STRING);
+        expect(data.sender_public_key).toStrictEqual(KEYPAIR_ALICE.publicKey);
+    });
+
+    test('encrypt stream with symmetric key', async () => {
+        const stream = new SigncryptStream(KEYPAIR_ALICE, [
+            new SymmetricKeyRecipient(Buffer.alloc(32), SYMMETRIC_KEY_BOB),
+        ]);
+
+        const result: Buffer[] = [];
+
+        await new Promise((rs, rj) => {
+            stream.on('error', rj);
+            stream.on('end', rs);
+            stream.on('data', chunk => result.push(chunk));
+
+            stream.end(INPUT_STRING);
+        });
+
+        expect(Buffer.concat(result)).toStrictEqual(SIGNCRYPTED_SYMMETRIC);
+    });
+
+    test('decrypt stream with symmetric key', async () => {
+        const stream = new DesigncryptStream(
+            new SymmetricKeyRecipient(Buffer.alloc(32), SYMMETRIC_KEY_BOB),
+            KEYPAIR_ALICE.publicKey);
+
+        const result: Buffer[] = [];
+
+        await new Promise((rs, rj) => {
+            stream.on('error', rj);
+            stream.on('end', rs);
+            stream.on('data', chunk => result.push(chunk));
+
+            stream.end(SIGNCRYPTED_SYMMETRIC);
+        });
+
+        expect(result.toString()).toBe(INPUT_STRING);
+        expect(stream.sender_public_key).toStrictEqual(KEYPAIR_ALICE.publicKey);
+    });
+
+    test('decrypt with wrong key fails', async () => {
+        await expect(async () => {
+            await designcrypt(SIGNCRYPTED,
+                new SymmetricKeyRecipient(Buffer.alloc(32), SYMMETRIC_KEY_MALLORY));
+        }).rejects.toThrow();
+    });
 });
